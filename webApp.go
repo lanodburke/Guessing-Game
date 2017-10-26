6985ab53541ee8307e16b1a6aa16b1dc3083942e
DONAL BURKE - Student <G00337729@gmit.ie>
2017-10-26 19:30:44 +0100

    finished problem 4 part 2 guess.tmpl

--- webApp.go
+++ webApp.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"net/http"
+	"html/template"
 )
+
+type msg struct {
+	Message string
+}
 
 func requestHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
 }
 
 func guessHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "guess.html")
+
+	message := &msg{Message: "Guess a number between 1 and 20"}
+	
+	s, _ := template.ParseFiles("guess.tmpl")
+
+	s.Execute(w, message)
 }
 
 func main() {
