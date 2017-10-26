9b0e1769a297cff5b88e19fb10edc555cf891487
DONAL BURKE - Student <G00337729@gmit.ie>
2017-10-26 19:49:18 +0100

    finish problem 5

--- webApp.go
+++ webApp.go
@@ -3,10 +3,18 @@ package main
 import (
 	"net/http"
 	"html/template"
+	"math/rand"
+	"time"
+	"strconv"
 )
 
 type msg struct {
 	Message string
+}
+
+func xrand(min, max int) int {
+    rand.Seed(time.Now().Unix())
+    return rand.Intn(max - min) + min
 }
 
 func requestHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,6 +22,18 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func guessHandler(w http.ResponseWriter, r *http.Request) {
+	myrand := xrand(1, 20)
+
+	mycookie, err := r.Cookie("target")
+	if(err == nil) {
+		target, _ := strconv.Atoi(mycookie.Value)
+
+		if target == 0 {
+			target = myrand
+		}
+	}
+
+	http.SetCookie(w, mycookie)
 
 	message := &msg{Message: "Guess a number between 1 and 20"}
 	
